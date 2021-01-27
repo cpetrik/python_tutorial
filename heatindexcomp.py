d409b6b3790c48281c49bb24e882d69d7bfa4801
Colleen Petrik <colleenpetrik@gmail.com>
2021-01-27 12:48:54 -0800

    Refactoring

--- heatindexcomp.py
+++ heatindexcomp.py
@@ -1,3 +1,5 @@
+from readdata import read_data
+
 # Create a column dictionary (names and indices to read)
 columns = {'date':0, 'time':1, 'tempout':2, 'humout':5, 'heatindex':13}
 
@@ -10,21 +12,7 @@ for column in columns:
     data[column] = []
 
 # Read the data file
-filename = "data/wxobs20170821.txt"
-
-with open(filename,'r') as datafile:
-    # Read first 3 lines (header)
-    for _ in range(3):
-        datafile.readline()
-
-    # Read and parse the rest of the file
-    for line in datafile:
-        split_line = line.split()
-        for column in columns:
-            i = columns[column]
-            t = types.get(column,str)
-            value = t(split_line[i])
-            data[column].append(value)
+data = read_data(columns, types)
 
 # Compute heat index
 def compute_heatindex(t,hum):
