6a41a0e31746f261570718ec80fb2d5ee8a9b9d2
Colleen Petrik <colleenpetrik@gmail.com>
2021-02-10 12:46:49 -0800

    Adding setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name = "mysci",
+    version = "1.0.0",
+    description = "A sample package",
+    author = "C.M. Petrik",
+    author_email = "colleenpetrik@gmail.com",
+    packages = ["mysci"],
+    install_requires = [])
