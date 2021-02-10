0409c01671efda72e213f93641562771e93244c4
Colleen Petrik <colleenpetrik@gmail.com>
2021-02-10 12:26:01 -0800

    Code for computing dewpoint temp

---
+++ dewpointtempcomp.py
@@ -0,0 +1,23 @@
+from mysci.readdata import read_data
+from mysci.printing import print_comparison
+from mysci.computation import compute_dewpoint
+
+# Create a column dictionary (names and indices to read)
+columns = {'date':0, 'time':1, 'tempout':2, 'humout':5, 'dewpt':6}
+
+# Data types for each colun (only if not string)
+types = {'tempout':float, 'humout':float, 'dewpt':float}
+
+# Read the data file
+data = read_data(columns, types=types)
+
+# Running the function to compute wci
+dewpointtemp = [compute_dewpoint(t,h) for t, h in zip(data['tempout'],
+    data['humout'])]
+
+# Output comparison of data
+print_comparison('DEW PT', data['date'], data['time'], data['dewpt'], 
+    dewpointtemp)
+
+#DEBUG
+
